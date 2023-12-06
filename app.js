66698289a50038eda74cd485a804854fda8c7df6
Aaron S <grayjackel5@gmail.com>
2023-12-06 12:39:54 -0800

    building structure

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+const express = require("express");
+const app = express();
+
+// define all endpoints here
+
+app.use(express.static("public"));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
+
+app.get("/hello", function (req, res) {
+  res.type("text");
+  res.send("Hello World!");
+});
