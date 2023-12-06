e5a2a26574579298191d5151ac2c58b3a32f4905
Aaron S <grayjackel5@gmail.com>
2023-12-06 13:14:20 -0800

    local host working

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const app = express();
 
 app.use(express.static("public"));
 const PORT = process.env.PORT || 8000;
-app.listen(PORT);
-//sp
 
 app.get("/hello", function (req, res) {
   // res.set("Content-Type", "text/plain");
