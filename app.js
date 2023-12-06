49bcb168fd7564d629a0b2704a7cc725c47055fa
Aaron S <grayjackel5@gmail.com>
2023-12-06 13:53:18 -0800

    I don't know what I am doing but I am on slide 8

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 "use strict";
 const express = require("express");
+const sqlite3 = require("sqlite3");
+const sqlite = require("sqlite");
+
 const app = express();
 
 // define all endpoints here
@@ -15,6 +18,11 @@ app.get("/hello", function (req, res) {
   // res.set("Content-Type", "text/plain");
   res.type("text"); // same as above
   res.send("You visited the Hello Route!");
+
+  // Extract the value of the 'key' query parameter
+  //   localhost:8000/hello?key=value
+  let value = req.query["key"];
+  res.send(`Value: ${value}`);
 });
 
 app.get("/hello/:name", function (req, res) {
@@ -23,6 +31,11 @@ app.get("/hello/:name", function (req, res) {
   // prints out name to screen
   // example http://localhost:8000/hello/aaron
   res.send("Hello " + req.params.name + "!");
+});
+
+app.get("/hello/name/:first/:last", function (req, res) {
+  let firstName = req.params["first"];
+  let lastName = req.params["last"];
 });
 
 app.get("/states/:state/cities/:city", function (req, res) {
@@ -50,3 +63,12 @@ app.listen(PORT, () => {
   //   console.log("Listening on port"+ PORT + "..."); // eqv
   console.log(`Listening on port ${PORT} ...`);
 });
+
+// somewhere in your app.js (after all // endpoint definitions)
+async function getDBConnection() {
+  const db = await sqlite.open({
+    filename: "<db-file-name>.db",
+    driver: sqlite3.Database,
+  });
+  return db;
+}
