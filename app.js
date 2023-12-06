095643020d978e9fa8ca68950be5d167453a012c
Aaron S <grayjackel5@gmail.com>
2023-12-06 12:50:50 -0800

    save point 1

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT);
 
 app.get("/hello", function (req, res) {
-  res.type("text");
+  // res.set("Content-Type", "text/plain");
+  res.type("text"); // same as above
   res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
